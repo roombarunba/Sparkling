9d13b2dd463a773528846e053c799fada4fa7185
nttr <nttr@BI-23560.local>
2018-02-13 15:11:18 +0900

    AlertDialogがAppCompoに依存していたため依存しない奴に修正

--- app/src/main/java/com/example/nttr/sparkling/TitleActivity.java
+++ app/src/main/java/com/example/nttr/sparkling/TitleActivity.java
@@ -2,6 +2,7 @@ package com.example.nttr.sparkling;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,7 +13,6 @@ import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
